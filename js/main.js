a599fcaa0b98b865dc131997fd90298f20a8c44e
SeungMiL <tmskqj@naver.com>
2021-12-15 23:46:58 +0900

    javascript_basic_2

--- js/main.js
+++ js/main.js
@@ -8,6 +8,28 @@ function openBox(hole){
     document.getElementById('alert').style.display = hole;
 }
 
-function closeBox(){
-    document.getElementById('alert').style.display = 'none';
+
+
+function plus(num){
+    2 + num
 }
+
+plus(3)
+
+
+var a = 3;
+
+a = 5;
+
+console.log(a);
+
+let b = 3;
+
+console.log(b);
+
+
+let b = 5;
+console.log(b);
+
+$('#test').addClass('yellow-bg');
+
