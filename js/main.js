269404e2763d886a1b16a5d46eb968b25a276c88
SeungMiL <tmskqj@naver.com>
2021-12-17 23:47:53 +0900

    javascript_basic_3

--- js/main.js
+++ js/main.js
@@ -1,35 +1,34 @@
-document.getElementById('hello').innerHTML = '안녕';
-document.getElementById('hi').innerHTML = '올때바밤바';
-document.getElementById('hi').style.fontSize = '50px';
+// document.getElementById('hello').innerHTML = '안녕';
+// document.getElementById('hi').innerHTML = '올때바밤바';
+// document.getElementById('hi').style.fontSize = '50px';
 
 
 
-function openBox(hole){
-    document.getElementById('alert').style.display = hole;
-}
+// function openBox(bts,this){
+//     document.getElementById('alert').style.display = bts;
+//     document.getElementById('alert').style.fontSize = this;
+// }
 
 
 
-function plus(num){
-    2 + num
-}
+// function plus(num){
+//     2 + num
+// }
 
-plus(3)
-
-
-var a = 3;
-
-a = 5;
-
-console.log(a);
-
-let b = 3;
-
-console.log(b);
+// plus(3)
 
 
-let b = 5;
-console.log(b);
+// var a = 3;
 
-$('#test').addClass('yellow-bg');
+// a = 5;
 
+// console.log(a);
+
+
+// $('#test').addClass('yellow-bg');
+
+logBtn = document.querySelector('.btn');
+
+logBtn.addEventListener('click', function(){
+    document.querySelector('.black-background').style.display = 'block';
+})
