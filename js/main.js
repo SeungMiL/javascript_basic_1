485278ac174ff8cd1c4bad4f73b7c574c55e24c5
SeungMiL <tmskqj@naver.com>
2021-12-18 15:11:11 +0900

    javascript_basic_6

--- js/main.js
+++ js/main.js
@@ -77,12 +77,68 @@ $('.btn-danger').on('click', function(){
 
 
 
+function 삼육구게임(i){
+    if( i % 3 == 0){
+        console.log('박수');
+    } else {
+        console.log('통과');
+    }
+};
+
+삼육구게임(36);
+
+function 삼육구겜(i){
+    if(i % 3 == 0 && i % 9 !== 0){
+        console.log('박수');
+    } else if (i % 9 == 0){
+        console.log('박수x2');
+    } else {
+        console.log('통과');
+    }
+}
+
+삼육구겜(11);
+
+function 삼육구(i) {
+    const numStr = i.toString();
+    if (numStr.charAt(numStr.length-1) === '3' || numStr.charAt(numStr.length-1) === '6' || numStr.charAt(numStr.length-1) === '9' ) {
+        console.log('박수');
+    } else {
+        console.log('통과');
+    }
+}
+
+삼육구(58); 
+
+var 예금액 = 60000;
+var 미래예금액 = 0;
+
+if( 예금액 >= 50000){
+    미래예금액 = 예금액 * 1.2 * 1.2;
+} else {
+    미래예금액 = 예금액 * 1.15 * 1.15;
+}
+
+console.log(미래예금액);
+
+
+var 첫커피양 = 360;
+var 마신커피 = 0;
+var 리필양 = 첫커피양 * 2/3;
+var 리필양2 = 리필양 * 2/3;
+
+마신커피 = 첫커피양 + 리필양;
+마신커피 = 마신커피 + 리필양2;
 
 
 
+console.log(마신커피);
 
 
 
+$('#show-menu').on('click', function(){
+    $('.left-menu').animate({ marginLeft : '0px'});
+})
 
 
 
