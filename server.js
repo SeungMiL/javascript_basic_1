97e18a6837c3b24455663021938247fa587df7e1
SeungMiL <tmskqj@naver.com>
2022-01-01 16:30:11 +0900

    javascript_basic_19

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended : true}));
 
-app.use('/javascript practice 3',express.static(__dirname +'/public'));
+app.use(express.static(__dirname))
 
 app.listen(8080, function(){
     console.log('listening on 8080')
@@ -17,4 +19,15 @@ app.get('/practice2', function(req, res){
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html')
+});
+
+
+app.get('/write', function(req,res){
+    res.sendFile(__dirname + '/write.html')
+});
+
+
+app.post('/add', function(req, res){
+    res.send('전송완료')
+    console.log(req.body)
 });
